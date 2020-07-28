6776f6428e3a29e28d2229eee2e11a9dd9c77ab9
phamtrong2000 <phamvantrong.va@gmail.com>
2020-07-28 16:17:41 +0700

    edit pages

--- resources/views/Pages/Company/DS1.blade.php
+++ resources/views/Pages/Company/DS1.blade.php
@@ -2,4 +2,163 @@
 @section('content')
 
 
+
+<!-- Topbar -->
+<!-- End of Topbar -->
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">Danh sách công ty/doanh nghiệp tuyển dụng</h1>
+
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 style="color: #026b97 !important" class="m-0 font-weight-bold text-primary">Danh sách</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <div id="dataTable_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-6">
+                            <div class="dataTables_length" id="dataTable_length">
+                                <label>
+                                    Hiển thị:
+                                    <select name="dataTable_length" aria-controls="dataTable" class="custom-select custom-select-sm form-control form-control-sm">
+                                        <option value="10">10</option>
+                                        <option value="25">25</option>
+                                        <option value="50">50</option>
+                                        <option value="100">100</option>
+                                    </select> kết quả
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6">
+                            <div id="dataTable_filter" class="dataTables_filter">
+                                <label>
+                                    Tìm kiếm:
+                                    <input type="search" class="form-control form-control-sm" placeholder="" aria-controls="dataTable">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <table class="table table-bordered dataTable" id="dataTable" width="100%" cellspacing="0" role="grid" aria-describedby="dataTable_info" style="width: 100%;">
+                                <thead>
+                                    <tr role="row">
+                                        <th class="sorting_asc" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-sort="ascending" style="width: 58px;">Tên công ty</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" style="width: 4em !important">Yêu cầu/Tiêu chí</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" style="width: 3em !important;">Địa chỉ</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" style="width: 2em !important;">Liên hệ</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" style="width: 4em !important;">Mức lương</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" style="width: 4em !important;">Số lượng</th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" style="width: 4em !important;">Đãi ngộ</th>
+                                    </tr>
+                                </thead>
+                                <tfoot>
+                                    <tr>
+                                        <th rowspan="1" colspan="1">Tên công ty</th>
+                                        <th rowspan="1" colspan="1">Yêu cầu/Tiêu chí</th>
+                                        <th rowspan="1" colspan="1">Địa chỉ</th>
+                                        <th rowspan="1" colspan="1">Liên hệ</th>
+                                        <th rowspan="1" colspan="1">Mức lương</th>
+                                        <th rowspan="1" colspan="1">Số lượng</th>
+                                        <th rowspan="1" colspan="1">Đãi ngộ</th>
+                                    </tr>
+                                </tfoot>
+                                <tbody>
+                                    <tr role="row" class="even">
+                                        <td class="sorting_1">FPT SOFT</td>
+                                        <td>Fullstack</td>
+                                        <td>1 Đường Cầu Diễn</td>
+                                        <td>0843423234</td>
+                                        <td>upto 20 triệu đồng/tháng</td>
+                                        <td>5</td>
+                                        <td>oke</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <td class="sorting_1">FPT SOFT</td>
+                                        <td>Fullstack</td>
+                                        <td>1 Đường Cầu Diễn</td>
+                                        <td>0843423234</td>
+                                        <td>upto 20 triệu đồng/tháng</td>
+                                        <td>5</td>
+                                        <td>oke</td>
+                                    </tr>
+                                    <tr role="row" class="even">
+                                        <td class="sorting_1">FPT SOFT</td>
+                                        <td>Fullstack</td>
+                                        <td>1 Đường Cầu Diễn</td>
+                                        <td>0843423234</td>
+                                        <td>upto 20 triệu đồng/tháng</td>
+                                        <td>5</td>
+                                        <td>oke</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <td class="sorting_1">FPT SOFT</td>
+                                        <td>Fullstack</td>
+                                        <td>1 Đường Cầu Diễn</td>
+                                        <td>0843423234</td>
+                                        <td>upto 20 triệu đồng/tháng</td>
+                                        <td>5</td>
+                                        <td>oke</td>
+                                    </tr>
+                                    <tr role="row" class="even">
+                                        <td class="sorting_1">FPT SOFT</td>
+                                        <td>Fullstack</td>
+                                        <td>1 Đường Cầu Diễn</td>
+                                        <td>0843423234</td>
+                                        <td>upto 20 triệu đồng/tháng</td>
+                                        <td>5</td>
+                                        <td>oke</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12 col-md-5">
+                            <div class="dataTables_info" id="dataTable_info" role="status" aria-live="polite">Hiển thị 5 trên 10 kết quả</div>
+                        </div>
+                        <div class="col-sm-12 col-md-7">
+                            <div class="dataTables_paginate paging_simple_numbers" id="dataTable_paginate">
+                                <ul class="pagination">
+                                    <li class="paginate_button page-item previous disabled" id="dataTable_previous">
+                                        <a href="#" aria-controls="dataTable" data-dt-idx="0" tabindex="0" class="page-link">Trước</a>
+                                    </li>
+                                    <li class="paginate_button page-item active">
+                                        <a href="#" aria-controls="dataTable" data-dt-idx="1" tabindex="0" class="page-link">1</a>
+                                    </li>
+                                    <li class="paginate_button page-item ">
+                                        <a href="#" aria-controls="dataTable" data-dt-idx="2" tabindex="0" class="page-link">2</a>
+                                    </li>
+                                    <li class="paginate_button page-item ">
+                                        <a href="#" aria-controls="dataTable" data-dt-idx="3" tabindex="0" class="page-link">3</a>
+                                    </li>
+                                    <li class="paginate_button page-item ">
+                                        <a href="#" aria-controls="dataTable" data-dt-idx="4" tabindex="0" class="page-link">4</a>
+                                    </li>
+                                    <li class="paginate_button page-item ">
+                                        <a href="#" aria-controls="dataTable" data-dt-idx="5" tabindex="0" class="page-link">5</a>
+                                    </li>
+                                    <li class="paginate_button page-item ">
+                                        <a href="#" aria-controls="dataTable" data-dt-idx="6" tabindex="0" class="page-link">6</a>
+                                    </li>
+                                    <li class="paginate_button page-item next" id="dataTable_next">
+                                        <a href="#" aria-controls="dataTable" data-dt-idx="7" tabindex="0" class="page-link">Sau</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
 @endsection
--- resources/views/Registration/registration.blade.php
+++ resources/views/Registration/registration.blade.php
@@ -4,6 +4,11 @@
 <head>
     <title>Registration Page</title>
     <link rel="stylesheet" type="text/css" href="asset/CSS/Lib/bootstrap.min.css">
+    <style>
+        [required]{
+        /*border: 1px solid red;*/
+}
+    </style>
 </head>
 
 <body>
@@ -14,24 +19,24 @@
                     <h1>Thông tin đăng ký</h1>
                 </div>
                 <div class="panel-body">
-                    <form method="POST">
+                    <form method="POST" action="">
                         <div class="form-group">
                             <label for="name">Họ và tên</label>
-                            <input type="text" class="form-control" id="txtName" name="name" placeholder="Họ và tên...">
+                            <input type="text" class="form-control" id="txtName" required name="name" placeholder="Họ và tên...">
                         </div>
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input type="email" class="form-control" id="txtEmail" name="email" placeholder="Email">
+                            <input type="email" class="form-control" id="txtEmail" required name="email" placeholder="Email">
                         </div>
                         <div class="form-group">
                             <label for="password">Mật khẩu</label>
-                            <input type="password" class="form-control" id="txtPassword" name="password" placeholder="Mật khẩu">
+                            <input type="password" class="form-control" id="txtPassword" required name="password" placeholder="Mật khẩu">
                         </div>
                         <div class="form-group">
                             <label for="password2">Xác nhận</label>
-                            <input type="password" class="form-control" id="txtPassword2" name="confirm_password" placeholder="Nhập lại mật khẩu">
+                            <input type="password" class="form-control" id="txtPassword2" required name="confirm_password" placeholder="Nhập lại mật khẩu">
                         </div>
-                        <div class="form-group">
+                        <div class="form-group" required>
                             <label for="Category">Loại tài khoản</label>
                             <select class="form-control" name="category" id="txtCategory">Loại tài khoản
 								<option name="category"  value="3">Sinh viên</option>
@@ -43,7 +48,7 @@
                             <button class="btn btn-info">Gửi</button>
                             <button class="btn btn-danger">Canle</button>
                         </div>
-                        @csrf
+                        
                     </form>
                 </div>
                 <div class="panel-footer text-right">
