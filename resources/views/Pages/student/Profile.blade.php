bd59778ae3d9274b34b885ad5fc2b9adec911f8d
phamtrong2000 <phamvantrong.va@gmail.com>
2020-07-30 16:05:55 +0700

    hoàn thành xong chức năng cập nhật thông tin nhưng thiếu ảnh pvtrong

--- resources/views/Pages/student/Profile.blade.php
+++ resources/views/Pages/student/Profile.blade.php
@@ -32,10 +32,11 @@
 
                                 <label for="studentGender"> Giới tính:</label>
                                 <p></p>
-                                <input  id="txtStudentGender"  value="{{$student->gender}}" type="radio" class="form-check-inline" name="gender">Nam
-                                <input  id="txtStudentGender" value="{{$student->gender}}" type="radio" class="form-check-inline" name="gender">Nữ
-                                <input  id="txtStudentGender" value="{{$student->gender}}" type="radio" class="form-check-inline" name="gender">Other
-
+                                
+                                <input  id="txtStudentGender" <?php if($student->gender=="Nam"){echo "checked";}?>  value="Nam" type="radio" class="form-check-inline" name="gender">Nam
+                                <input  id="txtStudentGender" <?php if($student->gender=="Nữ"){echo "checked";}?> value="Nữ" type="radio" class="form-check-inline" name="gender">Nữ
+                                <input  id="txtStudentGender" <?php if($student->gender=="Khác"){echo "checked";}?> value="Khác" type="radio" class="form-check-inline" name="gender">Other
+                                
 
 
                                 <label for="emailOther">Địa chỉ email</label>
@@ -56,10 +57,10 @@
                             <div class="form-group">
                                 <label for="department">Khoa</label>
                                 <select value="{{$student->department}}" name="department" class="form-control" id="sel1">
-                                    <option>Công nghệ thông tin</option>
-                                    <option>Điện tử viễn thông</option>
-                                    <option>Vật lý kỹ thuật</option>
-                                    <option>Hàng không vũ trụ</option>
+                                    <option <?php if($student->department=="Công nghệ thông tin"){echo "selected";}?> value="Công nghệ thông tin"  name="department">Công nghệ thông tin</option>
+                                    <option  <?php if($student->department=="Điện tử viễn thông"){echo "selected";}?> value="Điện tử viễn thông" name="department">Điện tử viễn thông</option>
+                                    <option  <?php if($student->department=="Vật lý kỹ thuật"){echo "selected";}?> value="Vật lý kỹ thuật" name="department">Vật lý kỹ thuật</option>
+                                    <option  <?php if($student->department=="Hàng không vũ trụ"){echo "selected";}?> value="Hàng không vũ trụ" name="department">Hàng không vũ trụ</option>
                                 </select>
                             </div>
                             <label for="major">Ngành</label>
@@ -67,39 +68,39 @@
                             <div class="form-group">
                                 <label for="level">Bậc</label>
                                 <select value="{{$student->level}}" name="level" class="form-control" id="sel1">
-                                    <option >Cao đăng</option>
-                                    <option>Đại học</option>
-                                    <option>Cao học</option>
+                                    <option <?php if($student->level=="Cao đẳng"){echo "selected";}?> name="level" value="Cao đẳng" >Cao đăng</option>
+                                    <option <?php if($student->level=="Đại học"){echo "selected";}?> name="level" value="Đại học">Đại học</option>
+                                    <option <?php if($student->level=="Cao học"){echo "selected";}?> name="level" value="Cao học">Cao học</option>
                                 </select>
                             </div>
                             <div class="form-group">
                                 <label for="training-system">Hình thức đào tạo</label>
                                 <select  value="{{$student->trainingSystem}}" name="trainingSystem" class="form-control" id="sel1">
-                                    <option>Chính quy</option>
-                                    <option>Tại chức</option>
-                                    <option>Tại chỗ</option>
+                                    <option <?php if($student->trainingSystem=="Chính quy"){echo "selected";}?> name="trainingSystem" value="Chính quy">Chính quy</option>
+                                    <option <?php if($student->trainingSystem=="Tại chức"){echo "selected";}?> name="trainingSystem" value="Tại chức">Tại chức</option>
+                                    <option <?php if($student->trainingSystem=="Tại chỗ"){echo "selected";}?> name="trainingSystem" value="Tại chỗ">Tại chỗ</option>
 
                                 </select>
                             </div>
                             <div class="form-group">
                                 <label  for="training-program">Chương trình đào tạo</label>
                                 <select value="{{$student->trainingProgram}}" name="trainingProgram" class="form-control" id="sel1">
-                                    <option>Chuẩn</option>
-                                    <option>CLC</option>
-                                    <option>CLC (Thông tư 23)</option>
+                                    <option <?php if($student->trainingProgram=="Chuẩn"){echo "selected";}?> value="Chuẩn">Chuẩn</option>
+                                    <option <?php if($student->trainingProgram=="CLC"){echo "selected";}?> value="CLC">CLC</option>
+                                    <option <?php if($student->trainingProgram=="CLC (Thông tư 23)"){echo "selected";}?> value=""CLC (Thông tư 23)">CLC (Thông tư 23)</option>
                                 </select>
                             </div>
                             <div class="form-group">
                                 <label for="year-of-course">Khoá</label>
                                 <select value="{{$student->yearOfCourse}}" name="yearOfCourse" class="form-control" id="sel1">
-                                    <option>QH-2014</option>
-                                    <option>QH-2015</option>
-                                    <option>QH-2016</option>
-                                    <option>QH-2017</option>
-                                    <option>QH-2018</option>
-                                    <option>QH-2019</option>
-                                    <option>QH-2020</option>
-                                    <option>QH-2021</option>
+                                    <option <?php if($student->yearOfCourse=="QH-2014"){echo "selected";}?> name="yearOfCourse" value="QH-2014">QH-2014</option>
+                                    <option <?php if($student->yearOfCourse=="QH-2015"){echo "selected";}?> name="yearOfCourse" value="QH-2015">QH-2015</option>
+                                    <option <?php if($student->yearOfCourse=="QH-2016"){echo "selected";}?> name="yearOfCourse" value="QH-2016">QH-2016</option>
+                                    <option <?php if($student->yearOfCourse=="QH-2017"){echo "selected";}?> name="yearOfCourse" value="QH-2017">QH-2017</option>
+                                    <option <?php if($student->yearOfCourse=="QH-2018"){echo "selected";}?> name="yearOfCourse" value="QH-2018">QH-2018</option>
+                                    <option <?php if($student->yearOfCourse=="QH-2019"){echo "selected";}?> name="yearOfCourse" value="QH-2019">QH-2019</option>
+                                    <option <?php if($student->yearOfCourse=="QH-2020"){echo "selected";}?> name="yearOfCourse" value="QH-2020">QH-2020</option>
+                                    <option <?php if($student->yearOfCourse=="QH-2021"){echo "selected";}?> name="yearOfCourse" value="QH-2021">QH-2021</option>
                                 </select>
                             </div>
 
